30510a380d251aa71d1e7cbf1bf5241da5758b0b
Srinivasan Ramesh <sramesh@cs.uoregon.edu>
2022-04-29 06:17:06 +0000

    Add more files for reference during installation

---
+++ spack_environment_recipe/theta_sourceme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+module load cmake
+module swap PrgEnv-intel PrgEnv-gnu
+module swap cray-libsci/20.06.1 cray-libsci/20.03.1
+module load craype-hugepages8M
+module unload darshan
+. ~/spack/share/spack/setup-env.sh
+spack env activate ascent
+export CRAYPE_LINK_TYPE=dynamic
+export MPICH_GNI_NDREG_ENTRIES=1024
+export PATH=/home/sramesh/VIZ_SERVICE/ascent_microservice/build/examples:/home/sramesh/VIZ_SERVICE/AMR_WIND_INSTALL/bin:/home/sramesh/RADICAL/mongo/bin:/home/sramesh/VIZ_SERVICE/TAU_INSTALL/craycnl/bin/:$PATH
+export PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/home/sramesh/VIZ_SERVICE/ascent_microservice/build/lib/pkgconfig:$PKG_CONFIG_PATH
+export RADICAL_LOG_LVL=DEBUG
+export RADICAL_PROFILE=TRUE
+export RADICAL_PILOT_DBURL=mongodb://rct:jdWeRT634k@thetalogin3.tmi.alcf.anl.gov:6874/rct_db
+source $HOME/.miniconda3/bin/activate
+conda activate rct
